0f195005e528e4baaa54691d0ea6fd3da0c6cd4a
Tuan Pham Minh <tuanpmhd@gmail.com>
2024-10-02 16:09:06 +0700

    fix: decompress multiple ixs for spl token

--- app/services/compression/decompressSplToken.ts
+++ app/services/compression/decompressSplToken.ts
@@ -15,19 +15,14 @@ import * as splToken from "@solana/spl-token";
 const rpcUrl = process.env.NEXT_PUBLIC_SOLANA_RPC_URL || clusterApiUrl('mainnet-beta');
 const connection: Rpc = createRpc(rpcUrl, rpcUrl);
 
-export const buildDecompressSplTokenTx = async (payer: string, mintAddress: string, inputAccounts: ParsedTokenAccount[]): Promise<Transaction> => {
+export const buildDecompressSplTokenTx = async (payer: string, mintAddress: string, compressedTokenAccounts: ParsedTokenAccount[]): Promise<Transaction> => {
     try {
         // Fetch the latest blockhash
         const { blockhash } = await connection.getLatestBlockhash();
         const transaction = new Transaction();
 
-        // Fetch recent validity proof for all input accounts
-        const proof = await connection.getValidityProof(
-            inputAccounts.map(account => bn(account.compressedAccount.hash)),
-        );
-
         // Group input accounts by their amounts
-        const amountGroups = inputAccounts.reduce((acc, account) => {
+        const amountGroups = compressedTokenAccounts.reduce((acc, account) => {
             const amount = account.parsed.amount.toNumber();
             if (amount > 0) {
                 if (!acc[amount]) acc[amount] = [];
@@ -37,10 +32,21 @@ export const buildDecompressSplTokenTx = async (payer: string, mintAddress: stri
         }, {} as { [key: number]: ParsedTokenAccount[] });
 
         // Create a transaction instruction for each amount group
-        for (const [amount, accounts] of Object.entries(amountGroups)) {
+        for (const [amount, compressedTokenAccounts] of Object.entries(amountGroups)) {
+            // Select accounts to transfer from based on the transfer amount
+            const [inputAccounts] = selectMinCompressedTokenAccountsForTransfer(
+                compressedTokenAccounts,
+                amount,
+            );
+
+            // Fetch recent validity proof for all input accounts
+            const proof = await connection.getValidityProof(
+                inputAccounts.map(account => bn(account.compressedAccount.hash)),
+            );
+
             const decompressIx = await CompressedTokenProgram.decompress({
                 payer: new PublicKey(payer), // The payer of the transaction.
-                inputCompressedTokenAccounts: accounts, // compressed token accounts of payer.
+                inputCompressedTokenAccounts: inputAccounts, // compressed token accounts of payer.
                 toAddress: await splToken.getAssociatedTokenAddress(
                     new PublicKey(mintAddress),
                     new PublicKey(payer)
