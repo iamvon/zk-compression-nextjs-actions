ba067693bc1d7c38c706f0e187ce4d09560ea86a
Tuan Pham Minh <tuanpmhd@gmail.com>
2024-10-06 15:39:22 +0700

    chore: better wording

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { IconBrandGithub } from "@tabler/icons-react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-24">
-      <p>Compress or Decompress USDC Actions</p>
+      <p>USDC ZK Compression Actions</p>
     </main>
   );
 }
